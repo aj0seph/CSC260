397ff3f8352f0053c222efe43ede03cb0aa40697
aj0seph <Ashley.Joseph@trojans.dsu.edu>
2023-04-23 23:50:44 -0400

    CSC260 Project - WIP
    CSC260

--- WeatherProject/Program.cs
+++ WeatherProject/Program.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace WeatherProject
 {
     internal static class Program
     {
+        static string getStringFromUrl(HttpClient client, string url)
+        {
+            var asyncResult = client.GetStringAsync(url);
+            asyncResult.Wait();
+            return asyncResult.Result;
+        }
+
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            HttpClient client = new HttpClient();
+            //client.GetStringAsync("https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=38369a3a213f4172a8d26a15284ddbc1&include=minutely");
+            //client.getStringFromUrl("https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=38369a3a213f4172a8d26a15284ddbc1&include=minutely");
+            string httpResult = getStringFromUrl(client, "https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=38369a3a213f4172a8d26a15284ddbc1&include=minutely");
+            WeatherApi.container convertedResult = JsonConvert.DeserializeObject<WeatherApi.container>(httpResult);
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
---
+++ WeatherProject/WeatherApi.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace WeatherProject
+{
+    internal class WeatherApi
+    {
+        //public class 
+        /*
+        public class WeatherInfo
+        {
+            public float app_temp { get; set; }
+        }
+        */
+
+        
+        public class WeatherInfo 
+        {
+            public float app_temp { get; set;}
+            public int aqi { get; set; }
+            public string city_name { get; set; }
+            public int clouds { get; set; }
+            public string country_code { get; set; }
+            public string datetime { get; set;}
+            public double dewpt { get; set; }
+            public double dhi { get; set; }
+            public double dni { get; set; }
+            public double elev_angle { get; set; }
+            public double ghi { get; set; }
+            public string gust { get; set; }
+            public double h_angle { get; set; }
+            public double lat { get; set; }
+            public double lon { get; set; }
+            public string ob_time { get; set; }
+            public string pod { get; set; }
+            public int precip { get; set; }
+            public double pres { get; set; }
+            public int rh { get; set; }
+            public double slp { get; set; }
+            public int snow { get; set; }
+            public double solar_rad { get; set; }
+            public string[] sources {get; set; }
+            public weather weather { get; set; }
+        }
+        
+
+        public class weather
+        {
+            public string icon { get; set; }
+            public string description { get; set; }
+            public int code{ get; set; }
+        }
+        //attributes of the classes
+        //contains everything we've created above
+        public class container
+        {
+            public int count { get; set; }
+            //public WeatherInfo[] data { get; set; }
+            public List<WeatherInfo> data { get; set; }
+            //public List <weather> weather { get; set;} 
+        }
+
+    }
+}
