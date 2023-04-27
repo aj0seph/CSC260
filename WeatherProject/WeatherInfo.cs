bc0adfa6ee6072003b583c7090debeeeccfcda10
aj0seph <Ashley.Joseph@trojans.dsu.edu>
2023-04-27 17:53:56 -0400

    WIP

--- WeatherProject/WeatherInfo.cs
+++ WeatherProject/WeatherInfo.cs
@@ -6,9 +6,59 @@ using System.Threading.Tasks;
 
 namespace WeatherProject
 {
-    internal class WeatherData
+    internal class WeatherInfo
     {
-        public class data 
+
+        public class coord 
+        {
+            public double lon { get; set; }
+            public double lat { get; set; }
+       
+        }
+
+        public class weather 
+        {
+                public string main { get; set; }
+
+                public string description { get; set; }
+                public string icon { get; set; }
+        }
+        public class main 
+        {
+            public double temp { get; set; }
+            public double pressure { get; set; }     
+            public double humidity { get; set; }
+        
+        }
+        public class wind 
+        {
+            public double speed { get; set; }   
+        
+        }
+
+        public class sys 
+        {
+            public long sunrise { get; set; }
+            public long sunset { get; set; }
+        
+        }
+
+        public class root 
+        {
+            public coord coord { get; set; }
+            public List<weather> weather { get; set; }
+            public main main { get; set; }
+
+            public wind wind { get; set; }
+
+            public sys sys { get; set; }
+
+
+        }
+
+        }
+
+        /*public class data 
         {
             double app_temp { get; set;}
             double aqi { get; set; }
@@ -53,4 +103,5 @@ namespace WeatherProject
         }
 
     }
+        */
 }
